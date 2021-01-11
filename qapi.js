8f175ef1f9486425573799b35f69d10d70198590
Wurikos <50768680+Wurikos@users.noreply.github.com>
2021-01-11 19:14:45 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,46 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5ffc78a4d97277001a088184",
+				"5ffc78a4d97277001a088186",
+				"5ffc78c66d6deb001f91af55",
+				"5ffc78c8f4d20b0022059796"
+			]
+		},
+		"5ffc78a4d97277001a088184": {
+			"id": "5ffc78a4d97277001a088184",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5ffc78a4d97277001a088186": {
+			"id": "5ffc78a4d97277001a088186",
+			"name": "index",
+			"pageUrl": "index",
+			"styles": {
+				"background": "#9f2626"
+			}
+		},
+		"5ffc78c66d6deb001f91af55": {
+			"id": "5ffc78c66d6deb001f91af55",
+			"pageUrl": "gffggff",
+			"name": "one more page",
+			"styles": {
+				"background": "#1d49c6"
+			}
+		},
+		"5ffc78c8f4d20b0022059796": {
+			"id": "5ffc78c8f4d20b0022059796",
+			"pageUrl": "fgfdgfdg",
+			"name": "page dosent know",
+			"styles": {
+				"background": "#279f46"
+			}
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/global-page-styles.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { transformVar } from "@quarkly/atomize";
+import { createGlobalStyle } from "styled-components";
+
+const pageStyles = {
+    "index": {
+        "background": "#9f2626"
+    },
+    "gffggff": {
+        "background": "#1d49c6"
+    },
+    "fgfdgfdg": {
+        "background": "#279f46"
+    }
+};
+
+const PageStyles = createGlobalStyle`
+    body {
+        ${({ styles }) =>
+            Object.entries(styles || {}).map(
+                ([prop, value]) => `${prop}: ${transformVar(prop, value)};`
+            )}
+    }
+`;
+export const GlobalQuarklyPageStyles = ({ pageUrl }) => <PageStyles styles={pageStyles[pageUrl]} />
